d5df84deba1f86cfa76773f154d82ae5f8b865bb
Lucie Chevalier <chevalierlucie81@gmail.com>
2022-08-15 12:54:07 +0200

    Stupid coaching

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Generic syntax:
+  # verb "path", to: "controller#action"
+  root to: "pages#home"
+  get "/about", to: "pages#about"
+  get "/contact", to: "pages#contact"
+  
+  get "/ask", to: "questions#ask"
+  get "/answer", to: "questions#answer"
 end
